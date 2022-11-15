817dc769951c58e385d2a4e52c6b86c4a60acd5e
José Romary Brandão <joseromarybrandao@gmail.com>
2022-11-15 15:44:24 -0300

    created summary component

--- src/Pages/Transactions/index.tsx
+++ src/Pages/Transactions/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../../Components/Header";
+import { Summary } from "../../Components/Summary";
 
 export function Transaction() {
     return (
         <div>
             <Header />
+            <Summary />
         </div>
     )
 }
